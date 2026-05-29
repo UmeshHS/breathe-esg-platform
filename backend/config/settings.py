68c483c17239a25726aead6ec86d6c3984660d7b
UmeshHS <uhs260066@gmail.com>
2026-05-30 00:11:58 +0530

    Fix Django storages configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,6 +140,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
